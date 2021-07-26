1da3ac82b2d1f0869f867a39070f05c255bdd053
Aleksandr Semerenko <sanya96088475@gmail.com>
2021-07-26 14:09:12 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.35369277eabbfb9f41d8e1c2d600fc7c.js"
+  "./precache-manifest.51cc323982b9ad7fc79a3fe043745a41.js"
 );
 
 self.addEventListener('message', (event) => {
